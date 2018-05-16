b683d9c3745209e887289d9df7f04d2d5d61e8e7
yoshi0202 <kobayashi020202@gmail.com>
2018-05-16 22:41:36 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+* 特定の呪文に反応するサンプルコード
+*/
+
+// discord.js モジュールのインポート
+const Discord = require('discord.js');
+
+// Discord Clientのインスタンス作成
+const client = new Discord.Client();
+
+// トークンの用意
+const token = 'NDQ2Mjk3MDMzMTMzNzg1MDkx.Dd2-Fg.jA3ovUOZsVrIlnXaKAud5SMc-T8';
+
+// 準備完了イベントのconsole.logで通知黒い画面に出る。
+client.on('ready', () => {
+    console.log('ready...');
+});
+
+
+// メッセージがあったら何かをする
+client.on('message', message => {
+    // メッセージの文字列による条件分岐
+    if (message.content === 'さっ') {
+
+        let channel = message.channel;
+        let author = message.author.username;
+        let reply_text = `ワイトもそう思います。`;
+
+        // そのチェンネルにメッセージを送信する
+        channel.reply(reply_text)
+            .then(message => console.log(`Sent message: ${reply_text}`))
+            .catch(console.error);
+        return;
+    }
+});
+
+
+// Discordへの接続
+client.login(token);
