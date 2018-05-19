a9b9a999d56b03964a3830076d83faff55ff6865
yoshiki kobayashi <kobayashi020202@gmail.com>
2018-05-19 19:57:52 +0900

    commit

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ const client = new Discord.Client();
 // トークンの用意
 const token = 'NDQ2Mjk3MDMzMTMzNzg1MDkx.Dd2-Fg.jA3ovUOZsVrIlnXaKAud5SMc-T8';
 
-// 準備完了イベントのconsole.logで通知黒い画面に出る。
-client.on('ready', () => {
-    console.log('ready...');
-});
-
-
 // メッセージがあったら何かをする
 client.on('message', msg => {
     // メッセージの文字列による条件分岐
@@ -25,13 +19,9 @@ client.on('message', msg => {
         let reply_text = `ワイトもそう思います。`;
 
         msg.reply(reply_text);
-        // そのチェンネルにメッセージを送信する
-        // channel.reply(reply_text)
-        //     .then(message => console.log(`Sent message: ${reply_text}`))
-        //     .catch(console.error);
-        // return;
     }
 });
+
 
 // Discordへの接続
 client.login(token);
